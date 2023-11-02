d1c5e6615aaa2fffb8aafa276aaa65348fe96974
rainbyc <work.rainier@gmail.com>
2023-11-03 00:31:45 +0800

    Fix the code on script file for the interest-details and add styling to the footer

--- script.js
+++ script.js
@@ -1,23 +1,52 @@
+
 document.addEventListener("DOMContentLoaded", function () {
-    const expandButtons = document.querySelectorAll(".expand-button-two");
-    const extraDetailsList = document.querySelectorAll(".key-features");
+    const expandButtons = document.querySelectorAll(".expand-button");
+    const extraDetailsList = document.querySelectorAll(".interest-details");
 
     expandButtons.forEach((button, index) => {
-        const buttonText = button.querySelector("p");
         const chevronIcon = button.querySelector(".fa-chevron-down");
 
         button.addEventListener("click", () => {
             // Toggle visibility of the extra details
             extraDetailsList[index].classList.toggle("visible");
 
-            // Toggle the text and icon
+            // Toggle the arrow direction
             if (extraDetailsList[index].classList.contains("visible")) {
-                buttonText.textContent = "Less details";
                 chevronIcon.classList.replace("fa-chevron-down", "fa-chevron-up");
             } else {
-                buttonText.textContent = "More details";
                 chevronIcon.classList.replace("fa-chevron-up", "fa-chevron-down");
             }
         });
     });
-});
+
+    const expandButtons2 = document.querySelectorAll(".expand-button-2");
+    const extraDetailsList2 = document.querySelectorAll(".key-features");
+
+    expandButtons2.forEach((button, index) => {
+        const chevronIcon = button.querySelector(".fa-chevron-down");
+
+        button.addEventListener("click", () => {
+            // Toggle visibility of the extra details
+            extraDetailsList2[index].classList.toggle("visible");
+
+            // Toggle the arrow direction
+            if (extraDetailsList2[index].classList.contains("visible")) {
+                chevronIcon.classList.replace("fa-chevron-down", "fa-chevron-up");
+            } else {
+                chevronIcon.classList.replace("fa-chevron-up", "fa-chevron-down");
+            }
+        });
+    });
+}); 
+
+
+
+
+
+
+
+
+
+
+
+
