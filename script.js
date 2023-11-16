2c8d2915969e320bbaac8890b1107e2605f1f82e
rainbyc <work.rainier@gmail.com>
2023-11-17 02:00:31 +0800

    Change the bank-list table, removed the key features column and will just add it on Details column

--- script.js
+++ script.js
@@ -18,35 +18,7 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         });
     });
-
-    const expandButtons2 = document.querySelectorAll(".expand-button-2");
-    const extraDetailsList2 = document.querySelectorAll(".key-features");
-
-    expandButtons2.forEach((button, index) => {
-        const chevronIcon = button.querySelector(".fa-chevron-down");
-
-        button.addEventListener("click", () => {
-            // Toggle visibility of the extra details
-            extraDetailsList2[index].classList.toggle("visible");
-
-            // Toggle the arrow direction
-            if (extraDetailsList2[index].classList.contains("visible")) {
-                chevronIcon.classList.replace("fa-chevron-down", "fa-chevron-up");
-            } else {
-                chevronIcon.classList.replace("fa-chevron-up", "fa-chevron-down");
-            }
-        });
-    });
 }); 
-
-
-
-
-
-
-
-
-
 
 
 
